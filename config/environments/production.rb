4d9bbba3b1287937ccbfb6aa06c0ccb659b3b6d6
Chad Crawford <ccrawforduwlax@gmail.com>
2015-06-14 22:00:08 -0500

    preprare app for Heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,10 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { host: 'https://limitless-meadow-9999.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
@@ -77,4 +81,5 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  Rails.application.routes.default_url_options[:host] = 'domain.com'
 end
